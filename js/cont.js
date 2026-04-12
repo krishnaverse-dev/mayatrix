411405067457915f578b7d53f25d818b4dbe5a3f
vaidik map and research <bramhaatma@gmail.com>
2026-04-12 13:28:41 +0545

    Update cont.js

--- js/cont.js
+++ js/cont.js
@@ -1,6 +1,6 @@
 function sendEmail() {
     const subject = encodeURIComponent("subject: write");
     const body = encodeURIComponent("pranam, sundarsath ji");
-    const email = "bramhaatma@gmail.com";
+    const email = "mayatrixnepal@gmail.com";
     window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
 }
