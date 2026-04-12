39a92561a79fe2f27a8b3c219b9631d22a231bed
vaidik map and research <bramhaatma@gmail.com>
2026-04-12 13:25:51 +0545

    Update email address in sendEmail function

--- js/cont उर्दू.js
+++ js/cont उर्दू.js
@@ -1,6 +1,6 @@
 function sendEmail() {
     const subject = encodeURIComponent("موضوع: لکھیں");
     const body = encodeURIComponent("آداب، سندرساتھ جی");
-    const email = "bramhaatma@gmail.com";
+    const email = "mayatrixnepal@gmail.com";
     window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
 }
